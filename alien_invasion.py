3725f5f84d75d8a6e2b3329399b702b1b7a9ceb9
zhuzhengxing <zhuzhengxing@mt-hirisun.com>
2020-12-15 16:01:49 +0800

    2020.12.15 update

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
+from alien import Alien
 
 
 def run_game():
@@ -17,14 +19,22 @@ def run_game():
     # 设置背景颜色
     bg_color = (230, 230, 230)
 
-    # 创建一艘飞船
-    ship = Ship(ai_settings,screen)
+    # 创建一艘飞船、一个子弹和一个外星人编组
+    ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的数组
+    bullets = Group()
+    aliens = Group()
+
+    # 创建一个外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏主循环
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_aliens(aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+from bullet import Bullet
+
+
 class Settings:
     """存储《外星人入侵》的所有设置的类"""
 
@@ -10,3 +13,16 @@ class Settings:
 
         # 飞船的位置
         self.ship_speed_factor = 1.5
+
+        # 子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+        # 外星人设置
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed=10
+        #fleet_direction为1表示向右，-1表示向左移
+        self.fleet_direction=1
